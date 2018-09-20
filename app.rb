4d2c29bd7cf0a610af25849a001b3bb9a6bc9d3e
Tomas Toledo <trtoledo1977@gmail.com>
2018-09-20 04:24:35 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -8,14 +8,16 @@ module FormsLab
       erb :'/pirates/index'
     end
 
+    get '/pirates/new' do
+      erb '/pirates/new'
+    end
+
     get '/pirates/:id' do
       @pirate = Pirate.find(params[:id])
       erb :'/pirates/show'
     end
 
-    get '/pirates/new' do
-      erb '/pirates/new'
-    end
+
 
 
 
