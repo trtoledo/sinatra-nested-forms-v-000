4f7518356eb4f70d7d8f3de2de90621b6aaa5e6a
Tomas Toledo <trtoledo1977@gmail.com>
2018-09-20 05:16:52 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,4 +1,6 @@
 require './environment'
+require_relative 'app/models/pirate.rb'
+require_relative 'app/models/ship.rb'
 
 module FormsLab
   class App < Sinatra::Base
@@ -18,7 +20,7 @@ module FormsLab
     end
 
     post '/pirates' do
-      
+
     end
 
 
