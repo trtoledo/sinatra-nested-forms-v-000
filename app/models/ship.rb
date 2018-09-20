3e7a987cbed9a4e45eae9989726c85166342c8e7
Tomas Toledo <trtoledo1977@gmail.com>
2018-09-20 05:44:48 +0000

    automatically backed up by learn

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,20 +1,20 @@
 class Ship
   attr_accessor :name, :type, :booty
 
-  @ships = []
+  @@all = []
 
   def initialize(args)
     @name = args[:name]
     @type = args[:type]
     @booty = args[:booty]
-    ships <= self
+    @@all <= self
   end
 
   def self.all
-    @ships
+    @@all
   end
 
   def self.clear
-    @ships = []
+    @@all = []
   end
 end
